3595010138af0caf8c74629d3b9854aaf7bb7ab4
Sören Henning <post@soeren-henning.de>
2017-11-10 21:26:33 +0100

    minor fixes

--- computer-graphics-project/shaders/per_fragment_illumination.f.glsl
+++ computer-graphics-project/shaders/per_fragment_illumination.f.glsl
@@ -88,6 +88,7 @@ void main() {
 
 
 	/*
+	// Some temporary code for gausisan blur
 	float offset[5] = float[]( 0.0, 1.0, 2.0, 3.0, 4.0 );
 	float weight[5] = float[]( 0.2270270270, 0.1945945946, 0.1216216216, 0.0540540541, 0.0162162162 );
 	if (material_texture_intensity) {
